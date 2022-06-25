88f323ac8e0be84ae9728f5a55f12b70c4555ddd
Prachigur99 <prachigurjar70@gmail.com>
2022-06-25 18:09:49 +0530

    weathercommit

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "4ca4c3a4445980afd8da97f09b083367"
+    }
+}
+
+module.exports = constants;
